7b5bdd4b60b016b972dbb64a83c162f7530f631d
modernjava-coach <amruthasuri.coach@gmail.com>
2025-01-06 20:12:01 +0530

    Create JSON Files in the Local File System for storage

--- src/main/java/com/fx_data_api/connectors/openexchagerates/connectflow/OpenExchangesRatesConnectorFlow.java
+++ src/main/java/com/fx_data_api/connectors/openexchagerates/connectflow/OpenExchangesRatesConnectorFlow.java
@@ -1,6 +1,10 @@
 package com.fx_data_api.connectors.openexchagerates.connectflow;
 
+import java.io.File;
 import java.time.Duration;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,8 +13,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.dsl.IntegrationFlowBuilder;
 import org.springframework.integration.dsl.Pollers;
+import org.springframework.integration.file.FileWritingMessageHandler;
 import org.springframework.integration.http.dsl.Http;
+import org.springframework.integration.http.dsl.HttpMessageHandlerSpec;
+import org.springframework.messaging.MessageHandler;
 import org.springframework.messaging.support.GenericMessage;
 
 @Configuration
@@ -18,12 +26,24 @@ public class OpenExchangesRatesConnectorFlow {
 
 	private static final Logger logger = LoggerFactory.getLogger(OpenExchangesRatesConnectorFlow.class);
 
+	private static final String FILE_EXTENSION = ".json";
+
+	private static final String FILE_NAME_PREFIX = "OPENEXCHANGERATES_";
+
+	private static final String FILE_NAME_DATE_FORMAT = "yyyy-MM-dd'T'HH-mm-ss'Z'";
+
 	@Value("${OPEN_EXCHANGE_RATES_API_URL}")
 	private String API_URL;
-	
+
 	@Value("${OPEN_EXCHANGE_RATES_APP_ID}")
 	private String APP_ID;
+
+	@Value("${OPEN_EXCHANGE_RATES_JSON_DIR}")
+	private String exchangeRatesJsonDir;
 	
+	@Value("${OPEN_EXCHANGE_RATES_API_POLL_SECONDS}")	
+	private Long exchangeRatesAPIPollSeconds;
+
 	@Bean
 	public IntegrationFlow exchangeRatesPollingFlow() {
 		// Validate that appId is provided
@@ -37,9 +57,27 @@ public class OpenExchangesRatesConnectorFlow {
 		logger.debug("API Endpoint URI: {}", uri);
 
 		return IntegrationFlow
-				.from(() -> new GenericMessage<>(""), c -> c.poller(Pollers.fixedRate(Duration.ofHours(1))))
-				.handle(Http.outboundGateway(uri).httpMethod(HttpMethod.GET).expectedResponseType(String.class))
-				.handle(message -> logger.debug("Exchange Rates Response: {}", message.getPayload())).get();
+				.from(() -> new GenericMessage<>(""), c -> c.poller(Pollers.fixedDelay(Duration.ofSeconds(exchangeRatesAPIPollSeconds))))
+				.handle(apiOutboundGatewayHandler(uri))
+                .handle(jsonFileWritingMessageHandler())
+                .get();
 
+	}
+
+	private MessageHandler apiOutboundGatewayHandler(String uri) {
+		return Http.outboundGateway(uri).httpMethod(HttpMethod.GET).expectedResponseType(String.class).getObject();
+	}
+	
+	private MessageHandler jsonFileWritingMessageHandler() {
+		
+		FileWritingMessageHandler fileWritingMessageHandler = new FileWritingMessageHandler(new File(exchangeRatesJsonDir));
+        
+		fileWritingMessageHandler.setFileNameGenerator(  message -> {
+                    String fileName = ZonedDateTime.now(ZoneOffset.UTC)
+                            .format(DateTimeFormatter.ofPattern(FILE_NAME_DATE_FORMAT));
+                    return FILE_NAME_PREFIX + fileName + FILE_EXTENSION;
+                });
+		
+		return fileWritingMessageHandler;
 	}
 }
